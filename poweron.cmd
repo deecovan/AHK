876199204d1780229ef1578df04d061ed472bfa0
Dee <dee.covan@gmail.com>
2025-01-04 18:02:13 +0200

    poweron.cmd poweroff.cmd

--- poweron.cmd
+++ poweron.cmd
@@ -1,5 +1,7 @@
 powercfg /s 6781f8b6-d310-490a-941d-85800813dc7b
-QRes.exe /x:1366 /y:768
 netsh interface set interface "Wi-Fi" admin=enable
-timeout /t 5 /nobreak
+pause
 ping youtube.com
+pause
+ping youtube.com
+pause
