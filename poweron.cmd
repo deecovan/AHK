4000b58dc4fd3c77dab878bf92c61e6ba7283785
Dee <dee.covan@gmail.com>
2025-01-07 05:55:25 +0200

    Update poweron.cmd Set q1366 on

--- poweron.cmd
+++ poweron.cmd
@@ -1,8 +1,9 @@
-q1366.bat
+QRes.exe /x:1366 /y:768
 cls
 powercfg /s 6781f8b6-d310-490a-941d-85800813dc7b
 netsh interface set interface "Wi-Fi" admin=enable
-timeout /t 05
-ping youtube.com
-timeout /t 05
-ping bing.com
+timeout /t 05 /nobreak
+ping youtube.com -n 1
+timeout /t 05 /nobreak
+ping bing.com -n 1
+pause
