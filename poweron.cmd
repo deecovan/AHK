d0cfef205efbb99e6aa5ecbaad1bbfee102baea3
Dee <dee.covan@gmail.com>
2025-03-11 05:40:02 +0200

    poweron.cmd

--- poweron.cmd
+++ poweron.cmd
@@ -2,8 +2,8 @@
 @rem cls
 @rem powercfg /s 6781f8b6-d310-490a-941d-85800813dc7b
 netsh interface set interface "Wi-Fi" admin=enable
-timeout /t 05 /nobreak
-ping youtube.com -n 1
-timeout /t 05 /nobreak
-ping bing.com -n 1
+@rem timeout /t 05 /nobreak
+ping youtube.com -n 5
+@rem timeout /t 05 /nobreak
+ping bing.com -n 5
 pause
