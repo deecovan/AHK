737bbc0e3a24ab8f6fc9058b9d2e0b2d843721d9
Dee <dee.covan@gmail.com>
2025-03-19 13:16:05 +0200

    Forget poweron and poweroff scripts

--- poweron.cmd
+++
@@ -1,9 +0,0 @@
-@rem QRes.exe /x:1366 /y:768
-@rem cls
-@rem powercfg /s 6781f8b6-d310-490a-941d-85800813dc7b
-netsh interface set interface "Wi-Fi" admin=enable
-@rem timeout /t 05 /nobreak
-ping youtube.com -n 5
-@rem timeout /t 05 /nobreak
-ping bing.com -n 5
-pause
