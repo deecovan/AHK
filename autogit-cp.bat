ddfd6f5cc9503bff90c3c1dace80222843ea2f47
Dee <dee.covan@gmail.com>
2025-06-20 21:33:54 +0300

    del C:\windows\q1*.bat and q1360.bat

--- autogit-cp.bat
+++ autogit-cp.bat
@@ -1,4 +1,5 @@
 del C:\windows\autogit*.* /Q
+del C:\windows\q1*.bat /Q
 copy *.bat C:\windows\ /Y
 copy *.cmd C:\windows\ /Y
 copy gamma*.* C:\windows\ /Y
---
+++ q1360.bat
@@ -0,0 +1,2 @@
+C:\Programs\QRes.exe /x:1360 /y:768
+cls
